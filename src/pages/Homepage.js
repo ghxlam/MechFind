49458a80f7ffe4da3ee32a0beab333988625ef6f
Ghulam Ahmed <noorghulam7153@gmail.com>
2024-12-23 13:42:00 -0500

    refactored code and updated imports, also worked on header

---
+++ src/pages/Homepage.js
@@ -0,0 +1,23 @@
+import '../css/Homepage.css';
+
+function Homepage() {
+  return (
+    <div className="Homepage">
+      <header className="Homepage-header">
+        <div class="wrapper">
+          <a href="a" class="Homepage-nav">
+            <div class="one"></div>
+            <div class="two"></div>
+            <div class="three"></div>
+          </a>
+        </div>
+        <h1>MechFind</h1>
+      </header>
+      <div>
+        This is the homepage. Under Construction.
+      </div>
+    </div>
+  );
+}
+
+export default Homepage;
